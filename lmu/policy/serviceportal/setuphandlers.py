93abf225f7ac6e5796634d8dc123056220b87a1a
Alexander Loechel <Alexander.Loechel@lmu.de>
2015-04-22 22:12:17 +0200

    umbau setuphandler

--- lmu/policy/serviceportal/setuphandlers.py
+++ lmu/policy/serviceportal/setuphandlers.py
@@ -5,13 +5,41 @@ from plone import api
 
 def setupDemoContent(context):
     """Install Demo Content on Portal"""
-    pass
+    return # seems to be called all the time.
     portal = api.portal.get()
 
+    import ipdb; ipdb.set_trace()
     if not hasattr(portal, 'umfragen'):
         polls_folder = api.content.create(
             type='Folder',
             title='Umfragen',
             container=portal)
 
-    #import ipdb; ipdb.set_trace()
+    if not hasattr(portal, 'blog-mit'):
+        blog_folder = api.content.create(
+            type='Blog Folder',
+            title='Blog mit',
+            container=portal)
+
+
+def importDemoContent(context):
+    """Install Demo Content on Portal"""
+
+    if context.readDataFile('lmu.policy.serviceportal_content.txt') is None:
+        return
+
+    return # seems to be called all the time.
+    portal = api.portal.get()
+
+    import ipdb; ipdb.set_trace()
+    if not hasattr(portal, 'umfragen'):
+        polls_folder = api.content.create(
+            type='Folder',
+            title='Umfragen',
+            container=portal)
+
+    if not hasattr(portal, 'blog-mit'):
+        blog_folder = api.content.create(
+            type='Blog Folder',
+            title='Blog mit',
+            container=portal)
