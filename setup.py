2ddc7f52b4f9f01c8d43cb51be5c2b0f79514840
Alexander Pilz <pilz@syslab.com>
2015-10-05 21:11:59 +0200

    Add dependencies

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ setup(name='lmu.policy.serviceportal',
       version=version,
       description="A Plone policy for the ZUV-Serviceportal",
       long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.rst")).read(),
+      open(os.path.join("docs", "HISTORY.rst")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        ],
+          "Framework :: Plone",
+          "Programming Language :: Python",
+      ],
       keywords='Plone Policy',
       author='Alexander Loechel',
       author_email='Alexander.Loechel@lmu.de',
@@ -24,9 +24,34 @@ setup(name='lmu.policy.serviceportal',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
+          'lmu.contenttypes.blog',
+          'lmu.contenttypes.pinnwand',
+          'lmu.theme.serviceportal',
+          'pas.plugins.shibboleth_headers',
+          'Pillow',
+          'Plone',
+          'plone.api',
+          'Products.LongRequestLogger',
           'setuptools',
+          'z3c.jbot',
           # -*- Extra requirements: -*-
       ],
+      extras_require={
+          'test': [
+              'mock',
+              'plone.app.testing',
+              'plone.app.robotframework[debug]',
+              'fake-factory',
+          ],
+          'develop': [
+              'coverage',
+              'flake8',
+              'i18ndude',
+              'Sphinx',
+              'zptlint',
+          ],
+          'release': []
+      },
       entry_points="""
       # -*- Entry points: -*-
 
