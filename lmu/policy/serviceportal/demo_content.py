4fe917e16da2a43476adf95f5dd4b452b87902c0
Alexander Loechel <Alexander.Loechel@lmu.de>
2015-10-05 22:38:04 +0200

    demo users

--- lmu/policy/serviceportal/demo_content.py
+++ lmu/policy/serviceportal/demo_content.py
@@ -1,40 +1,22 @@
 # -*- coding: utf-8 -*-
 
 demo_users = {
-    'Brady': {
-        'email': 'demo@lmu.de',
-        'password': 'Test@IUK',
-        'properties': {
-            'fullname': 'Stephanie Brady',
-            'homepage': 'https://iukintest.verwaltung.uni-muenchen.de/personen/b/brady_stephanie/index.html',
-        },
-        'roles': ('Member', 'sp_members')
-    },
-    'Simon.Kirner': {
-        'email': 'demo@lmu.de',
-        'password': 'Test@IUK',
-        'properties': {
-            'fullname': 'Simon Kirner',
-            'homepage': 'https://iukintest.verwaltung.uni-muenchen.de/personen/k/kirner_simon/index.html',
-        },
-        'roles': ('Member', 'sp_members')
-    },
-    'Alexander.Loechel': {
+    '72C918A84D785B9F': {
         'email': 'Alexander.Loechel@lmu.de',
         'password': 'Test@IUK',
         'properties': {
             'fullname': 'Alexander Loechel',
             'location': 'Martiusstr. 4; München',
-            'homepage': 'https://iukintest.verwaltung.uni-muenchen.de/personen/l/loechel_alexander/index.html',
+            'homepage': '',
         },
         'roles': ('Member', 'Manager', 'cms-admins', 'in_sp_supportteam', 'sp_members')
     },
-    'Katharine.Linges': {
+    '5B4187806EA6E3B0': {
         'email': 'demo@lmu.de',
         'password': 'Test@IUK',
         'properties': {
             'fullname': 'Katharine Linges',
-            'homepage': 'https://iukintest.verwaltung.uni-muenchen.de/personen/l/linges_katharine/index.html',
+            'homepage': '',
         },
         'roles': ('Member', 'in_sp_supportteam', 'sp_members')
     }
@@ -45,7 +27,7 @@ demo_polls = {
         'poll_type': 'Star Poll',
         'title': 'Wie gut hilft Ihnen unser Service-Portal bei der Arbeit?',
         'description': 'Seien Sie ehrlich, nur so können wir besser werden.',
-        'author': 'Katharine.Linges',
+        'author': 'K5B4187806EA6E3B0',  # Katharine Linges
         'modification_date': '2014/10/02 12:43:00.000000 GMT+2',
         'state': 'open',
     }
